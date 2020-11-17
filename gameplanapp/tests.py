a20ac307e29ba0cedc6877b7353d2aafcac8bb75
RagDoll95 <anguspepperfinch@gmail.com>
2020-11-16 22:26:10 -0500

    Moved test file

--- gameplanapp/tests.py
+++ gameplanapp/tests.py
@@ -2,7 +2,7 @@ from datetime import date
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 # Create your tests here.
-from gameplanapp.models import Event, GameplanUser
+from gameplanapp.models import Event, GameplanUser, Friendship
 
 User = get_user_model()
 
@@ -28,6 +28,11 @@ class GameplanUserModelTest(TestCase):
         user = User.objects.get(username='test_user')
         test_gameplanuser = GameplanUser.objects.get(user=user)
         self.assertEqual(test_gameplanuser.user_dateofbirth, date.today())
+
+    def testProfile(self):
+        test_user=User.objects.get(username='test_user')
+        test_gameplanuser=GameplanUser.objects.get(user=test_user)
+        self.assertEqual(test_gameplanuser.user_bio, "Sample user bio")
 
 
 class EventModelTest(TestCase):
@@ -67,7 +72,6 @@ class JoinEventTest(TestCase):
         test_event = Event.objects.create(event_title="Test Event Title", event_location="Test Location",
                                           event_manager=User.objects.get(username="test_user1").gameplanuser, event_date=date.today())
         test_event.save()
-
         pass
 
     def tearDown(self):
@@ -85,6 +89,34 @@ class JoinEventTest(TestCase):
         test_event = Event.objects.get(event_title="Test Event Title")
         user = User.objects.get(username='test_user2')
         test_event.delete()
-        user.event_attending.remove(test_event)
+        user.gameplanuser.event_attending.remove(test_event)
         containsevent = test_event in user.gameplanuser.event_attending.all()
         self.assertFalse(containsevent)
+
+class FriendTest(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        test_user1 = User.objects.create(
+            username='test_user1', password='1X<ISRUkw+tuK')
+        test_user1.save()
+        test_user2 = User.objects.create(
+            username='test_user2', password='1X<ISRUkw+tuK')
+        test_user2.save()
+        test_event = Event.objects.create(event_title="Test Event Title", event_location="Test Location",
+                                          event_manager=User.objects.get(username="test_user1").gameplanuser, event_date=date.today())
+        test_event.save()
+
+        pass
+
+    def testaddFriend(self):
+        user1 = User.objects.get(username='test_user1')
+        user2 = User.objects.get(username='test_user2')
+        user1.gameplanuser.addfriend(second_user_id=user2.id)
+        test_Friend = Friendship.objects.get(friend_user=user1.gameplanuser, friend=user2.gameplanuser)
+        if(test_Friend.friend_user.user.username == 'test_user1'):
+            if(test_Friend.friend.user.username == 'test_user2'):
+                self.assertTrue(True)
+            else:
+                self.assertTrue(False)
+        else:
+            self.assertFalse(True)
